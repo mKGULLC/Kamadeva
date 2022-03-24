890a8e7aabf357333cdae76465fda537b00f6c90
mKGULLC <102265031+mKGULLC@users.noreply.github.com>
2022-03-23 22:41:22 -0400

    begin display data

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log("hi");
-
 const url = "data.xlsx"
 
 /* set up async GET request */
@@ -11,10 +9,9 @@ req.responseType = "arraybuffer"
 
 req.onload = function(e) {
   let workbook = XLSX.read(req.response)
-  console.log("data loaded")
-  /* DO SOMETHING WITH workbook HERE */
-  let jsa = XLSX.utils.sheet_to_json(worksheet, opts)
+  let jsa = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
   console.log(jsa)
+
 }
 
 req.send()
