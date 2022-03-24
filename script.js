7b3e9349a4ab05ec1e2214b47638297f796fe30d
mKGULLC <102265031+mKGULLC@users.noreply.github.com>
2022-03-23 22:10:23 -0400

    acquire data

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hi")
+'use strict';
+
+console.log("hi");
+
+// XLSX is a global from the standalone script
+
+(async() => {
+  const url = "data.xlsx";
+  const data = await (await fetch(url)).arrayBuffer();
+  /* data is an ArrayBuffer */
+  const workbook = XLSX.read(data);
+
+  /* DO SOMETHING WITH workbook HERE */
+})();
